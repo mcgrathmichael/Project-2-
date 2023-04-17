ff2dd0e9adb236dcba601878de5408a186692835
SachaWildCode <sacha.loumachi@gmail.com>
2023-04-17 13:15:54 +0200

    hotFix

--- frontend/src/components/StartButton/StartButton.jsx
+++ frontend/src/components/StartButton/StartButton.jsx
@@ -4,26 +4,13 @@ import "./StartButton.scss";
 function StartButton() {
   // The start button only appears when another button is clicked.
   // Animation should be smooth
-
-  const showStartButton = () => {
-    const startbtn = document.querySelector("#start");
-    startbtn.style.display = "block";
-  };
-
+  // Use a UseState to display the button not queryselector
   const OpenGamePage = () => {};
 
   return (
     <>
       {/* Once the user enters his name and chosses a theme and clicks the Start btn, 
     he can go the game route */}
-      <button
-        className="myButton"
-        type="submit"
-        id="StartBtn"
-        onClick={showStartButton}
-      >
-        theme
-      </button>
 
       {/* onClick, timer starts */}
       <button
