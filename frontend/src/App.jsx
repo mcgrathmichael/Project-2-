c8a9141fb8084fab325b9120781538827cde15c0
Michael_McGrath <corecreate0@gmail.com>
2023-04-17 13:47:47 +0200

    gameover pop up+rules page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import "./App.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
@@ -5,11 +6,15 @@ import Home from "./pages/Home";
 import Game from "./pages/Game";
 import Rules from "./pages/Rules";
 import LeaderboardPage from "./pages/LeaderboardPage";
+=======
+import RulesPage from "./rules_page/RulesPage";
+>>>>>>> b761501 (gameover pop up+rules page)
 
 function App() {
   // Only show StopWatch after 5 seconds
   return (
     <div className="App">
+<<<<<<< HEAD
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<NavBar />}>
@@ -20,6 +25,9 @@ function App() {
           </Route>
         </Routes>
       </BrowserRouter>
+=======
+      <RulesPage />
+>>>>>>> b761501 (gameover pop up+rules page)
     </div>
   );
 }
--- frontend/src/pages/Rules.jsx
+++ frontend/src/pages/Rules.jsx
@@ -1,5 +1,37 @@
-function Rules() {
-  return <h1>Rules</h1>;
-}
+import React from "react";
+import "./Rules.scss";
 
+function Rules() {
+  return (
+    <div className="container">
+      <div className="rules">
+        <h2 className="title">The Rules</h2>
+        <li className="rules_list">
+          <ul>
+            The aim of the game is to select all the duplicate cards till there
+            are no more!
+          </ul>
+          <ul>
+            Each round you will choose two cards and every time you click a card
+            it will be counted.
+          </ul>
+          <ul>
+            Choosing two of the same cards in a round will get you closer to the
+            end! When you start to get the hang of it we'll start making it more
+            challenging.
+          </ul>
+          <ul>
+            Keep an eye on your life! You only get X chances to make a pair or
+            else, you're out.
+          </ul>
+        </li>
+      </div>
+      <div className="button_div">
+        <button className="GoBack" type="button">
+          Go back
+        </button>
+      </div>
+    </div>
+  );
+}
 export default Rules;
