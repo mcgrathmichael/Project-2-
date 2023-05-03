1709304868e3d130ca2b12f3ba9e4f900a140e04
SachaWildCode <sacha.loumachi@gmail.com>
2023-05-03 16:09:33 +0200

    rules fixed css

--- frontend/src/pages/RulesPage/RulesPage.jsx
+++ frontend/src/pages/RulesPage/RulesPage.jsx
@@ -3,34 +3,18 @@ import "./RulesPage.scss";
 
 function RulesPage() {
   return (
-    <div className="container">
-      <div className="rules">
-        <h2 className="title">The Rules</h2>
-        <li className="rules_list">
-          <ul>
-            The aim of the game is to select all the duplicate cards till there
-            are no more!
-          </ul>
-          <ul>
-            Each round you will choose two cards and every time you click a card
-            it will be counted.
-          </ul>
-          <ul>
-            Choosing two of the same cards in a round will get you closer to the
-            end! When you start to get the hang of it we'll start making it more
-            challenging.
-          </ul>
-          <ul>
-            Keep an eye on your life! You only get X chances to make a pair or
-            else, you're out.
-          </ul>
-        </li>
-      </div>
-      <div className="button_div">
-        <button className="GoBack" type="button">
-          Go back
-        </button>
-      </div>
+    <div className="rules-container">
+      <h2 className="rules-title">The Rules</h2>
+      <p className="rules_list">
+        The aim of the game is to select all the duplicate cards till there are
+        no more! <br />
+        Each round you will choose two cards and every time you click a card it
+        will be counted. <br />
+        Choosing two of the same cards in a round will get you closer to the
+        end! <br />
+        When there is no time left, you losed the game, you need to finish it
+        before the timer ends!
+      </p>
     </div>
   );
 }
