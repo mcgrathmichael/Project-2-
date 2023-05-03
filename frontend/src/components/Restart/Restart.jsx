9431ee535d09e2840881ef7ee2a6aaf2a99e03b0
Khouloud Belkhir <koukabelkhir@gmail.com>
2023-05-03 17:07:17 +0200

    second commit

--- frontend/src/components/Restart/Restart.jsx
+++ frontend/src/components/Restart/Restart.jsx
@@ -4,12 +4,12 @@ function Restart() {
   };
 
   return (
-    <>
+    <div className="restart_container">
       {" "}
       <button type="submit" id="restart" onClick={restart}>
         Restart
       </button>
-    </>
+      </div>
   );
 }
 export default Restart;
