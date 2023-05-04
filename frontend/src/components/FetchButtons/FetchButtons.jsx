a250bc86494923647a7ef315dc22b352d8200dd8
Khouloud Belkhir <koukabelkhir@gmail.com>
2023-05-04 11:25:59 +0200

    animation for the title on home page and the loader are added

--- frontend/src/components/FetchButtons/FetchButtons.jsx
+++ frontend/src/components/FetchButtons/FetchButtons.jsx
@@ -112,9 +112,10 @@ function FetchButtons({ isReady }) {
 
   // Function to render the data from the API in the last return,  perhaps it could be
   // used to send image to other components ?
+  const Spinner = () => <div className="loader"></div>
   const renderApiData = () => {
     if (filteredData.length === 0 && fetched) {
-      return <span>loading</span>
+      return <span> {Spinner()}</span>
     }
     return (
       <div>
--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -13,7 +13,22 @@ function HomePage() {
 
   return (
     <>
-      <h1 className="header_text">Mashup Memo</h1>
+     <div className="container">
+        <div className="header_text">
+          <span style={{ '--i': -4 }}>M</span>
+          <span style={{ '--i': -3 }}>a</span>
+          <span style={{ '--i': -4 }}>s</span>
+          <span style={{ '--i': -1 }}>h</span>
+          <span style={{ '--i': 0 }}>u</span>
+          <span style={{ '--i': 1 }}>p</span>
+          <span></span>
+          <span style={{ '--i': 2 }}>M</span>
+          <span style={{ '--i': 3 }}>E</span>
+          <span style={{ '--i': 4 }}>M</span>
+          <span style={{ '--i': 4 }}>O</span>
+        </div>
+      </div>
+     
 
       <InputUserName isReady={isReady} />
       <FetchButtons isReady={ready} />
